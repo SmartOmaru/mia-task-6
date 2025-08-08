5c9db650d7234da6004a4b84773f522bba5377ce
unknown <omar2004j@gmail.com>
2025-08-08 20:14:36 +0300

    Implemented 'move to next box after typing' and backspace moves back and one character limit

--- gui.py
+++ gui.py
@@ -16,7 +16,9 @@ label.pack(pady=20)
 frame = tk.Frame(master, bg='black')
 frame.pack()
 
+entries = []
 for i in range(6):
+    entries_row = []
     for j in range(5):
         entry = tk.Entry(
             frame,
@@ -25,5 +27,33 @@ for i in range(6):
             justify='center',
             bg='grey')
         entry.grid(row=i, column=j, padx=5, pady=5)
+        entries_row.append(entry)
         
+        entry.bind("<KeyRelease>", lambda event, row=i, col=j: on_key(event, row, col))
+        
+    entries.append(entries_row)
+
+def on_key(event, row, col):
+   
+    box = event.widget
+    
+    if event.keysym == "BackSpace":
+        if col > 0:
+            entries[row][col - 1].focus()
+        box.delete(0, "end")
+        return
+        
+    typed = box.get().upper()
+    if len(typed) > 0:
+        box.delete(0, "end")
+        box.insert(0, typed[-1])
+        if col < 4:
+            entries[row][col + 1].focus()
+    
+
+        
+        
+        
+
+
 master.mainloop()
